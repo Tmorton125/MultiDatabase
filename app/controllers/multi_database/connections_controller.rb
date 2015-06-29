73a27672c705d914e3bb4d59bab1b02cfccdeec8
Tmorton125 <tim@Timothys-iMac.local>
2015-06-28 20:02:01 -0400

    New Page
    Adding New page with some styling to action list.

--- app/controllers/multi_database/connections_controller.rb
+++ app/controllers/multi_database/connections_controller.rb
@@ -6,5 +6,19 @@ module MultiDatabase
   	def index
   		@connections = MultiDatabase::DatabaseConnection.all
   	end
+
+  	def new
+  		@connection = MultiDatabase::DatabaseConnection.new
+  	end
+
+	def create
+		# Params contains the message object so create new instance of object
+		# on save successful save redirect to show action otherwise re-render new form with error messages
+
+        #TODO IMPLEMENT Create for now just redirect back to index
+
+		redirect_to action: :index
+	end
+
   end
 end
