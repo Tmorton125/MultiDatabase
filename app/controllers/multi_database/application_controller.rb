165a85d95e0cd85f23449d869a805eeac3555eb8
Tmorton125 <tim@Timothys-iMac.local>
2015-12-08 14:08:32 -0500

    Using Default Application Layout
    Fixed assets so they are now being properly loaded into main
    application in the correct order.
    
    Now Engine uses the Main Application Stylesheet

--- app/controllers/multi_database/application_controller.rb
+++ app/controllers/multi_database/application_controller.rb
@@ -1,5 +1,5 @@
 module MultiDatabase
   class ApplicationController < ::ApplicationController
-  	
+  	layout 'layouts/application'
   end
 end
--- config/manifest.rb
+++ config/manifest.rb
@@ -1,5 +1,6 @@
 {
   engine:     "MultiDatabase::Engine",
+  asset_folder: 'multi_database',
   navigation_title: "Database Management",
   links: [ { title: "Connection Management", url: "multidatabase.connections_path", order: 1},
            { title: "MySQL Databases", url: "multidatabase.mysql_databases_path", order: 2},
