ac6858507b098ea4e1d157d711669bab42fde961
Tmorton125 <tim@Timothys-iMac.local>
2015-12-08 17:09:09 -0500

    Adding mount configuration
    Adding Mount As configuration to manifest file.

--- config/manifest.rb
+++ config/manifest.rb
@@ -1,6 +1,7 @@
 {
   engine:     "MultiDatabase::Engine",
   asset_folder: 'multi_database',
+  route_as: 'multidatabase',
   navigation_title: "Database Management",
   links: [ { title: "Connection Management", url: "multidatabase.connections_path", order: 1},
            { title: "MySQL Databases", url: "multidatabase.mysql_databases_path", order: 2},
